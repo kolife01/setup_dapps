7f4d3730cf83396f1e9d1d644f0cd422d197c352
Kohei Tanaka <kohei69@gmail.com>
2019-02-11 14:39:10 +0900

    fix

--- src/js/contract_abi.js
+++ src/js/contract_abi.js
@@ -1,2 +1,33 @@
 //Replace [] with your contract ABI
-var contractABI =[];
+var contractABI =[
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_value",
+        "type": "string"
+      }
+    ],
+    "name": "set",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function",
+    "signature": "0x4ed3885e"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "get",
+    "outputs": [
+      {
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function",
+    "signature": "0x6d4ce63c"
+  }
+];
